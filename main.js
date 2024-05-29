95b148fa39d62b420493b18ce542926d0c3827f4
gt-plus <gtorrealba.plus@gmail.com>
2024-05-29 10:47:37 -0400

    240529-1047-G) Revisión

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import { Estudiante } from "./estudiante.js";
 
-let est1 = new Estudiante("Jorge", 2, 3, 4);
+let est1 = new Estudiante("Jorge Perez", 2, 3, 4);
 document.getElementById("salida").innerHTML = est1.notaFinal();
